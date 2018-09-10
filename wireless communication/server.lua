a3733f523d61cafd496b9a3e77e813503e9c4c99
Shikareth <gothic344@gmail.com>
2018-09-10 19:09:01 +0200

    '\t' => '__'

--- wireless communication/server.lua
+++ wireless communication/server.lua
@@ -1,5 +1,5 @@
 ----------------------------------------
---    		Wireless Mainframe 		  --
+--        Wireless Mainframe       --
 ----------------------------------------
 
 local component = require("component")
@@ -8,47 +8,47 @@ local event = require("event")
 local packets = {}
 
 function initializeServer(port)
-	if not component.modem.isOpen(port) then
-		if not component.modem.open(port) then
-			print("Failed to poen port. Check network card")
-			return
-		end
-		io.write("port: ", port, " is open\n")
-	end
-	print("Server started")
-	component.modem.broadcast(port, Server started")
+  if not component.modem.isOpen(port) then
+    if not component.modem.open(port) then
+      print("Failed to poen port. Check network card")
+      return
+    end
+    io.write("port: ", port, " is open\n")
+  end
+  print("Server started")
+  component.modem.broadcast(port, Server started")
 end
 
 function push(...)
-	local packet = {...}
-	local source = packet[3]
-	io.write("\n, source, "\t", packet[6])
-	table.insert(packets, {source, packet[6]})
+  local packet = {...}
+  local source = packet[3]
+  io.write("\n, source, "\t", packet[6])
+  table.insert(packets, {source, packet[6]})
 end
 
 function pull()
-	local packet = table.remove(packets, 1)
-	if packet[2] == "resetAE" then
-		os.execute(pwd.."proc/resetAE")
-	elseif packet[2] == "spawner" then
-		os.execute(pwd.."proc/spawner")
-	elseif packet[2] == "voidoreminer" then
-		os.execute(pwd.."proc/voidoreminer")
-	elseif packet[2] == "ack" then
-		computer.beep(1000)
-	end
+  local packet = table.remove(packets, 1)
+  if packet[2] == "resetAE" then
+    os.execute(pwd.."proc/resetAE")
+  elseif packet[2] == "spawner" then
+    os.execute(pwd.."proc/spawner")
+  elseif packet[2] == "voidoreminer" then
+    os.execute(pwd.."proc/voidoreminer")
+  elseif packet[2] == "ack" then
+    computer.beep(1000)
+  end
 end
 
 ----------------------------------------
--- 			  Main Procedure 		  --
+--         Main Procedure       --
 ----------------------------------------
 
 os.execute("clear")
 initializeServer(4216)
 repeat
-	local evt = table.pack(event.pull())
-	if evt[1] == modem_message" then
-		push(table.unpack(vet))
-		pull()
-	end
+  local evt = table.pack(event.pull())
+  if evt[1] == modem_message" then
+    push(table.unpack(vet))
+    pull()
+  end
 until evt[1] == "interrupted"
--- wireless communication/wireless_Redstone_microcontroller_driver.lua
+++ wireless communication/wireless_Redstone_microcontroller_driver.lua
@@ -2,144 +2,124 @@ local r = component.proxy(component.list("redstone")())
 local m = component.proxy(component.list("modem")())
 
 local server = "mac address"
-local sides = { bottom = 0, 
-				front = 1,
-				back = 2,
-				top = 3,
-				right = 4
-				left = 5 }
-local state = { bottom = false, 
-				front = false,
-				back = false,
-				top = false,
-				right = false
-				left = false }
+local sides = { bottom = 0, front = 1, back = 2, top = 3, right = 4, left = 5 }
+local state = { bottom = false,  front = false, back = false, top = false, right = false, left = false }
 
 ----------------------------------------
--- 				Functions 			  --
+--             Functions              --
 ----------------------------------------
 
 local function sleep(timeout)
-	local deadline = computer.uptime() + (timeout or 0)
-	repeat
-		computer.pullSignal(deadline - computer.uptime())
-	until computer.uptime() >= deadline
+  local deadline = computer.uptime() + (timeout or 0)
+  repeat
+    computer.pullSignal(deadline - computer.uptime())
+  until computer.uptime() >= deadline
 end
 
 local function enable(side)
-	if side == nil then
-		for k,v in pairs(state) do
-			state[k] = true
-			r.setOutput(sides[k], 15)
-		end
-	else
-		state[side] = true
-		r.setOutput(sides[side], 15)
-	end
+  if side == nil then
+    for k,v in pairs(state) do
+      state[k] = true
+      r.setOutput(sides[k], 15)
+    end
+  else
+    state[side] = true
+    r.setOutput(sides[side], 15)
+  end
 end
 
 local function disable(side)
-	if side == nil then
-		for k,v in pairs(state) do
-			state[k] = false
-			r.setOutput(sides[k], 0)
-		end
-	else
-		state[side] = true
-		r.setOutput(sides[side], 0)
-	end
+  if side == nil then
+    for k,v in pairs(state) do
+      state[k] = false
+      r.setOutput(sides[k], 0)
+    end
+  else
+    state[side] = true
+    r.setOutput(sides[side], 0)
+  end
 end
 
 local function timer(side, time, mode)
-	if mode then
-		enable(side)
-		sleep(time)
-		disable(side)
-	else
-		disable(side)
-		sleep(time)
-		enable(side)
-	end
+  if mode then
+    enable(side)
+    sleep(time)
+    disable(side)
+  else
+    disable(side)
+    sleep(time)
+    enable(side)
+  end
 end
 
 local function toogle(side)
-	if side == nil then
-		for k,v in pairs(state) do
-			state[k] = not state[k]
-			if state[k]
-				enable(k)
-			else
-				disable(k)
-			end
-		end
-	else
-		state[side] = not state[side]
-		if state[side]
-			enable(side)
-		else
-			disable(side)
-		end
-	end
+  if side == nil then
+    for k,v in pairs(state) do
+      state[k] = not state[k]
+      if state[k]
+        enable(k)
+      else
+        disable(k)
+      end
+    end
+  else
+    state[side] = not state[side]
+    if state[side]
+      enable(side)
+    else
+      disable(side)
+    end
+  end
 end
 
 local function stateToBinary()
-	local sum = 0
-	for k,v in pairs(state) do
-		local b = 0
-		if v then b = 1 end
-		sum = sum + b * math.pow(2,(sides[k]))
-	end
-	return sum
-end	
+  local sum = 0
+  for k,v in pairs(state) do
+    local b = 0
+    if v then b = 1 end
+    sum = sum + b * math.pow(2,(sides[k]))
+  end
+  return sum
+end  
 
 local function toBoolean(string)
-	if string == "true" then
-		return true
-	elseif string == "false" then
-		return false
-	else
-		return nil
-	end
+  if string == "true" then
+    return true
+  elseif string == "false" then
+    return false
+  else
+    return nil
+  end
 end
 
 function openPort(port)
-	if not m.isOpen(port) then	
-		m.open(port)
-		computer.beep(1000)
-		computer.beep(2000)
-		computer.beep(1000)
-	end
+  if not m.isOpen(port) then  
+    m.open(port)
+    computer.beep(1000)
+    computer.beep(2000)
+    computer.beep(1000)
+  end
 end
 
 ----------------------------------------
--- 			Main Routine 			  --
+--           Main Routine             --
 ----------------------------------------
 
 openPort(4216)
 
 repeat
-	local evt = table.pack(computer.pullSignal())
-	if evt[1] == "modem_message" and evt[3] == server then 
-		if evt[6] == "enable" then
-			enable(evt[7])
-		elseif evt[6] == "disable" then
-			disable(evt[7])
-		elseif evt[6] == "toggle" then
-			toggle(evt[7])
-		elseif evt[6] == "timer" then
-			timer(evt[7], tonumber(evt[8]), toBoolean(evt[9]))
-		end
-		m.send(evt[3], 4216, tostring(stateToBinary()))
-		computer.beep(100)
-	end
+  local evt = table.pack(computer.pullSignal())
+  if evt[1] == "modem_message" and evt[3] == server then 
+    if evt[6] == "enable" then
+      enable(evt[7])
+    elseif evt[6] == "disable" then
+      disable(evt[7])
+    elseif evt[6] == "toggle" then
+      toggle(evt[7])
+    elseif evt[6] == "timer" then
+      timer(evt[7], tonumber(evt[8]), toBoolean(evt[9]))
+    end
+    m.send(evt[3], 4216, tostring(stateToBinary()))
+    computer.beep(100)
+  end
 until evt[1] == "interrupted"
-
-
-
-
-
-
-
-
-
-
