9dd3d8bbaecad78da769511d10cf282d8172a637
Shikareth <gothic344@gmail.com>
2018-09-11 21:32:25 +0200

    release wireless redstone transmitter

--- wireless communication/wireless_Redstone_microcontroller_driver.lua
+++ wireless communication/wireless_Redstone_microcontroller_driver.lua
@@ -1,7 +1,8 @@
-local r = component.proxy(component.list("redstone")())
 local m = component.proxy(component.list("modem")())
+local r = component.proxy(component.list("redstone")())
 
 local server = "mac address"
+local port = 4216
 local sides = { bottom = 0, front = 1, back = 2, top = 3, right = 4, left = 5 }
 local state = { bottom = false,  front = false, back = false, top = false, right = false, left = false }
 
@@ -16,79 +17,95 @@ local function sleep(timeout)
   until computer.uptime() >= deadline
 end
 
-local function enable(side)
-  if side == nil then
-    for k,v in pairs(state) do
-      state[k] = true
-      r.setOutput(sides[k], 15)
-    end
+function state2number(x)
+  if x then
+    return 15
+  elseif not x then
+    return 0
   else
-    state[side] = true
-    r.setOutput(sides[side], 15)
+    return nil
   end
 end
 
-local function disable(side)
-  if side == nil then
-    for k,v in pairs(state) do
-      state[k] = false
-      r.setOutput(sides[k], 0)
-    end
-  else
-    state[side] = true
-    r.setOutput(sides[side], 0)
+function toboolean(x)
+  if type(x) == "number" then
+    if x == 1 then return true end
+    if x == 0 then return false end
+    return nil
+  elseif type(x) == "string" then
+    if x == "true" then return true end
+    if x == "false" then return false end
+    return nil
   end
+  assert(false, "invalid type")
 end
 
-local function timer(side, time, mode)
-  if mode then
-    enable(side)
-    sleep(time)
-    disable(side)
-  else
-    disable(side)
-    sleep(time)
-    enable(side)
-  end
-end
-
-local function toogle(side)
-  if side == nil then
-    for k,v in pairs(state) do
-      state[k] = not state[k]
-      if state[k]
-        enable(k)
-      else
-        disable(k)
+function enable(t)
+  if t[1] == nil then
+    for k,v in pairs(sides) do
+      if k ~= "n" then
+        state[k] = true
+        r.setOutput(v,15)
       end
     end
   else
-    state[side] = not state[side]
-    if state[side]
-      enable(side)
-    else
-      disable(side)
+    for k,v in pairs(t) do
+      if k ~= "n" then      
+        state[v] = true
+        r.setOutput(sides[v], 15)
+      end
     end
   end
 end
 
-local function stateToBinary()
-  local sum = 0
-  for k,v in pairs(state) do
-    local b = 0
-    if v then b = 1 end
-    sum = sum + b * math.pow(2,(sides[k]))
-  end
-  return sum
-end  
-
-local function toBoolean(string)
-  if string == "true" then
-    return true
-  elseif string == "false" then
-    return false
+function disable(t)
+  if t[1] == nil then
+    for k,v in pairs(sides) do
+      if k ~= "n" then
+        state[k] = false
+        r.setOutput(v,0)
+      end
+    end
   else
-    return nil
+    for k,v in pairs(t) do
+      if k ~= "n" then      
+        state[v] = false
+        r.setOutput(sides[v], 0)
+      end
+    end
+  end
+end
+
+function toggle(t)
+  if t[1] == nil then
+    for k,v in pairs(sides) do
+      if k ~= "n" then
+        state[k] = not state[k]
+        r.setOutput(v,state2number(state[k]))
+      end
+    end
+  else
+    for k,v in pairs(t) do
+      if k ~= "n" then      
+        state[v] = not state[v]
+        r.setOutput(sides[v], state2number(state[v]))
+      end
+    end
+  end
+end
+
+function timer(t)
+  local time = tonumber(t[1])
+  local mode = toboolean(t[2])
+  for i=1,2,1 do table.remove(t, 1) end
+  if mode then
+    enable(t)
+    sleep(time)
+    disable(t)
+  else
+    disable(t)
+    sleep(time)
+    enable(t)
   end
 end
 
@@ -102,24 +119,43 @@ function openPort(port)
 end
 
 ----------------------------------------
---           Main Routine             --
+--            Main Routine            --
 ----------------------------------------
 
-openPort(4216)
-
-repeat
+openPort(port)
+repeat 
   local evt = table.pack(computer.pullSignal())
-  if evt[1] == "modem_message" and evt[3] == server then 
-    if evt[6] == "enable" then
-      enable(evt[7])
-    elseif evt[6] == "disable" then
-      disable(evt[7])
-    elseif evt[6] == "toggle" then
-      toggle(evt[7])
-    elseif evt[6] == "timer" then
-      timer(evt[7], tonumber(evt[8]), toBoolean(evt[9]))
+  local type = evt[1]
+  local from = evt[3]
+  local cmd = evt[6]
+  local result = true
+  if type == "modem_message" then
+    for i=1,6,1 do
+      table.remove(evt, 1)
     end
-    m.send(evt[3], 4216, tostring(stateToBinary()))
-    computer.beep(100)
+    --print("Type: ",type)
+    --print("From: ",from)
+    --print("CMD : ",cmd)
+    --print(table.unpack(evt))
+    if cmd == "enable" then
+      enable(evt)
+    elseif cmd == "disable" then
+      disable(evt)
+    elseif cmd == "toggle" then
+      toggle(evt)
+    elseif cmd == "timer" then
+      if #evt < 2 then 
+        result = false
+      else
+        timer(evt)
+      end
+    else
+      result = false
+    end
+    if result then
+      m.send(tostring(from), port, "ack")
+    else
+      m.send(tostring(from), port, "error")
+    end
   end
-until evt[1] == "interrupted"
+until type == "interrupted"
